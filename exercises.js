045750d71fd26ff8e66c8e235e3c5d7f19f91ff2
Tomas Cravero <tomascravero25@gmail.com>
2020-10-25 20:35:16 -0300

    JS-018: 4) b)

--- exercises.js
+++ exercises.js
@@ -117,8 +117,35 @@ console.log("Array from May to November: ", monthsNew);
 
 var num = Math.random(0,1);
 if (num>=0,5){
-   console.log("Greater than 0,5");
+   alert("Greater than 0,5");
 }else{
-    console.log("Lower than 0,5");
+    alert("Lower than 0,5");
+}
+
+/* b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
+    i)“Bebe” si la edad es menor a 2 años
+    ii) “Nino” si la edad es entre 2 y 12 años
+    iii) “Adolecente” si la edad es entre 13 y 19 años
+    iv) “Joven” si la edad está entre 20 y 30 años
+    v) “Adulto” entre 31 y 60 años
+    vi) “Adulto mayor” entre 61 y 75 años
+    vii) “Anciano” si es mayor a 75 años
+ */
+
+ var Age = 55;
+if (Age < 2){
+    alert("Bebé");
+}else if ((Age > 2) && (Age < 12)){
+    alert("Niño");
+}else if ((Age > 13) && (Age < 19)){
+    alert("Adolescente");
+}else if ((Age > 20) && (Age < 30)){
+    alert("Joven");
+}else if ((Age > 31) && (Age < 60)){
+    alert("Adulto");
+}else if ((Age > 61) && (Age < 75)){
+    alert("Adulto Mayor");
+}else if (Age > 75){
+    alert("Anciano");
 }
 
