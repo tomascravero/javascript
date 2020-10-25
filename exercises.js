b01072c28eb2529f6704918351d3f0ad35196987
Tomas Cravero <tomascravero25@gmail.com>
2020-10-25 14:30:07 -0300

    JS-002: 1) b)

--- exercises.js
+++ exercises.js
@@ -1,7 +1,17 @@
-// exercise 1
+/*  1) Variables y Operadores
+    a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable. */
 
 var x = 10;
 var y = 15;
 var z;
-z = x + y;
-console.log(z);
+var z = x + y;
+console.log("El resultado de la suma es: ", z);
+
+/* b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
+
+var txt1 = "Hi! ";
+var txt2 = "How are you?";
+var phrase = txt1.concat(txt2);
+console.log (phrase);
+
+
