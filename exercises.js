aee94c6d9e7c0d493956e2da4696f5918022cb7c
Tomas Cravero <tomascravero25@gmail.com>
2020-10-25 14:58:43 -0300

    JS-003: 1) c)

--- exercises.js
+++ exercises.js
@@ -3,9 +3,8 @@
 
 var x = 10;
 var y = 15;
-var z;
 var z = x + y;
-console.log("El resultado de la suma es: ", z);
+console.log("The result of the sum is: ", z);
 
 /* b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
 
@@ -13,5 +12,25 @@ var txt1 = "Hi! ";
 var txt2 = "How are you?";
 var phrase = txt1.concat(txt2);
 console.log (phrase);
+
+/* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
+    guardando el resultado la suma una 3er variable (utilizar length). */
+
+var $text1 = "Learning Javascript";
+var $text2 = "with Radium Rocket";
+var large = 0;
+for (var i=0; i<$text1.length; i++) {
+    if ($text1[i] != " "){
+        large++;
+    }
+}
+for (var k=0; k<$text2.length; k++){
+    if ($text2[k] != " "){
+        large++;
+    }
+}
+console.log ("Length: ", large);
+
+/*  */
 
 
