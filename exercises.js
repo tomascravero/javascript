7445addb450186ddd57cd217ece3530502b2653f
Tomas Cravero <tomascravero25@gmail.com>
2020-10-25 20:21:50 -0300

    JS-017: 4) a)

--- exercises.js
+++ exercises.js
@@ -110,5 +110,15 @@ var monthsArray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio
 var monthsNew = monthsArray.slice(4,11);
 console.log("Array from May to November: ", monthsNew);
 
+/* 4 ) If Else 
+    a) Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), 
+    si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” 
+    y sino un alerta con el mensaje “Lower than 0,5” */
 
+var num = Math.random(0,1);
+if (num>=0,5){
+   console.log("Greater than 0,5");
+}else{
+    console.log("Lower than 0,5");
+}
 
