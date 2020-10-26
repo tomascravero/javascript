20820a41c787489b4969ac0fa38c98df6cc756b6
Tomas Cravero <tomascravero25@gmail.com>
2020-10-25 21:39:32 -0300

    JS-024: 6) b)

--- exercises.js
+++ exercises.js
@@ -189,8 +189,22 @@ console.log(empty);
     a) Crear una función suma que reciba dos valores numéricos y retorne el resultado.
     Ejecutar la función y guardar el resultado en una variable, mostrando el valor de dicha variable en la consola del navegador. */
 
-function suma(num1, num2){
+/*function suma(num1, num2){
     return num1 + num2;
 }
 _sumatoria = suma(5,10);
-console.log(_sumatoria); 
+console.log(_sumatoria); */
+
+/* b) A la función suma anterior, agregarle una validación para controlar si alguno de los parámetros no es un número, 
+    mostrar una alerta aclarando que uno de los parámetros tiene error y retornar el valor NaN como resultado. */
+
+function suma(num1, num2){
+    if (isNaN(num1) || isNaN(num2)){
+        return "NaN";
+    }else{
+        return num1 + num2;
+    }
+}
+_sumatoria = suma("H",10);
+console.log(_sumatoria);
+
