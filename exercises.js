af1c2d3af6007d2c05ad4f962c38c4285af2b431
Tomas Cravero <tomascravero25@gmail.com>
2020-10-25 21:07:31 -0300

    JS-022: 5) d)

--- exercises.js
+++ exercises.js
@@ -175,4 +175,12 @@ for (let h=0; h<fruits.length; h++){
 }
 alert(sentence);
 
+/* d) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, 
+    es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9.
+    Mostrar por la consola del navegador el al array final (utilizar console.log). */
 
+var empty = [];
+for (let h=0; h<10; h++){
+    empty[h] = h + 1;
+}
+console.log(empty);
