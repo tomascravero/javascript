61a3ecbfe12b6b5405483301689c19dfb7edcde6
Tomas Cravero <tomascravero25@gmail.com>
2020-10-26 09:38:34 -0300

    JS-027: 6) e)

--- exercises.js
+++ exercises.js
@@ -1,22 +1,22 @@
 /*  1) Variables y Operadores
     a) Crear dos variables numéricas y utilizar el operador suma para guardar el valor de la suma de ambos números en una 3er variable. */
 
-var x = 10;
+/*var x = 10;
 var y = 15;
 var z = x + y;
-console.log("The result of the sum is: ", z);
+console.log("The result of the sum is: ", z);*/
 
 /* b) Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable. */
 
-var txt1 = "Hi! ";
+/*var txt1 = "Hi! ";
 var txt2 = "How are you?";
 var phrase = txt1.concat(txt2);
-console.log (phrase);
+console.log (phrase);*/
 
 /* c) Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string) 
     guardando el resultado la suma una 3er variable (utilizar length). */
 
-var $text1 = "Learning Javascript";
+/*var $text1 = "Learning Javascript";
 var $text2 = "with Radium Rocket";
 var large = 0;
 for (var i=0; i<$text1.length; i++) {
@@ -34,34 +34,34 @@ console.log ("Length: ", large);
 /* 2) Strings
     a) Crear una variable de tipo string con al menos 10 caracteres y convertir todo el texto en mayúscula (utilizar toUpperCase).  */
 
-var _txt1 = "This is a text with at least 10 characters";
+/*var _txt1 = "This is a text with at least 10 characters";
 console.log(_txt1.toUpperCase());
 
 /* b) Crear una variable de tipo string con al menos 10 caracteres y 
     generar un nuevo string con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring). */
 
-var $t1 = "Description";
+/*var $t1 = "Description";
 var fiveC = $t1.substring(0,5);
 console.log(fiveC, "are the first 5 characters of: ", $t1);
 
 /* c) Crear una variable de tipo string con al menos 10 caracteres y 
 generar un nuevo string con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring). */
 
-var newT1 = "Programming";
+/*var newT1 = "Programming";
 var last3 = newT1.substring(8,11);
 console.log(last3, "are the last 3 characters of: ", newT1);
 
 /* d) Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en mayúscula y
     las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +). */
 
-var $textN = "pROGRammIng";
+/*var $textN = "pROGRammIng";
 var capitalize = $textN.substring(0,1).toUpperCase() + $textN.substring(1).toLowerCase();
 console.log("These is the text: ", $textN, "to capitalize: ", capitalize);
 
 /* e) Crear una variable de tipo string con al menos 10 caracteres y 
     algún espacio en blanco. Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf). */
 
-var _text = "Programming in Javascript";
+/*var _text = "Programming in Javascript";
 var space = _text.indexOf(" ", 0);
 console.log("The first space is in the position: ", space);
 
@@ -80,34 +80,33 @@ console.log(palabra);
     mostrar por consola los meses 5 y 11 (utilizar console.log) */
 
 var months = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-console.log("Month N° 5:", months[5]);
-console.log("Month N° 11:", months[11]);
+/*console.log("Month N° 5:", months[5]);
+console.log("Month N° 11:", months[11]);*/
 
 /* b) Ordenar el array de meses alfabéticamente y mostrarlo por consola (utilizar sort). */
 
-console.log("The list sorted alphabetically", months.sort());
+/*console.log("The list sorted alphabetically", months.sort());*/
 
 /* c) Agregar un elemento al principio y al final del array (utilizar unshift y push). */
 
-console.log(months.unshift("Year"), months.push("New Year"));
+/*console.log(months.unshift("Year"), months.push("New Year"));*/
 
 /* d) Quitar un elemento del principio y del final del array (utilizar shift y pop). */
 
-console.log(months.shift(), months.pop());
+/*console.log(months.shift(), months.pop());*/
 
 /* e) Invertir el orden del array (utilizar reverse) */
 
-console.log("Array inverted: ", months.reverse());
+/*console.log("Array inverted: ", months.reverse());*/
 
 /* f) Unir todos los elementos del array en un único string donde cada mes este separado por un guión - (utilizar join). */
 
-var $arrayJ = months.join("-");
-console.log($arrayJ);
+/*var $arrayJ = months.join("-");
+console.log($arrayJ);*/
 
 /* g) Crear una copia del array de meses que contenga desde Mayo hasta Noviembre (utilizar slice). */
 
-var monthsArray = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
-var monthsNew = monthsArray.slice(4,11);
+var monthsNew = months.slice(4,11);
 console.log("Array from May to November: ", monthsNew);
 
 /* 4 ) If Else 
@@ -115,12 +114,12 @@ console.log("Array from May to November: ", monthsNew);
     si el valor es mayor o igual que 0,5 mostrar una alerta con el mensaje “Greater than 0,5” 
     y sino un alerta con el mensaje “Lower than 0,5” */
 
-var num = Math.random(0,1);
+/*var num = Math.random(0,1);
 if (num>=0,5){
    alert("Greater than 0,5");
 }else{
     alert("Lower than 0,5");
-}
+}*/
 
 /* b) Crear una variable “Age” que contenga un número entero entre 0 y 100 y muestre los siguientes mensajes de alerta:
     i)“Bebe” si la edad es menor a 2 años
@@ -153,37 +152,37 @@ if (Age < 2){
     a) Crear un array que contenga 5 palabras y recorrer dicho array utilizando
     un bucle for de JavaScript para mostrar una alerta utilizando cada una de las palabras. */
 
-var fruits = ["apple", "pear", "banana", "strawberry", "peach"];
+/*var fruits = ["apple", "pear", "banana", "strawberry", "peach"];
 for (let h=0; h<fruits.length; h++){
     alert(fruits[h]);
-}
+}*/
 
 /* b) Al array anterior convertir la primera letra de cada palabra en mayúscula y mostrar una alerta por cada palabra modificada. */
 
-for (let h=0; h<fruits.length; h++){
+/*for (let h=0; h<fruits.length; h++){
     fruits[h] = fruits[h].substring(0,1).toUpperCase() + fruits[h].substring(1);
     alert(fruits[h]);
-}
+}*/
 
 /* c) Crear una variable llamada “sentence” que tenga un string vacío, luego al array del punto 
     a) recorrerlo con un bucle for para ir guardando cada palabra dentro de la variable sentence.
     Al final mostrar una única alerta con la cadena completa. */
 
-var sentence = [];
+/*var sentence = [];
 for (let h=0; h<fruits.length; h++){
     sentence[h] = fruits[h];
 }
-alert(sentence);
+alert(sentence);*/
 
 /* d) Crear una array vacío y con un bucle for de 10 repeticiones llenar el array con el número de la repetición, 
     es decir que al final de la ejecución del bucle for deberia haber 10 elementos dentro del array, desde el número 0 hasta al numero 9.
     Mostrar por la consola del navegador el al array final (utilizar console.log). */
 
-var empty = [];
+/*var empty = [];
 for (let h=0; h<10; h++){
     empty[h] = h + 1;
 }
-console.log(empty);
+console.log(empty);*/
 
 /* 6) Funciones 
     a) Crear una función suma que reciba dos valores numéricos y retorne el resultado.
@@ -224,7 +223,7 @@ console.log(num5);
 /* d) A la función suma del ejercicio 6b) agregarle una llamada que valide que los números sean enteros.
     En caso que haya decimales mostrar un alerta con el error y retorna el número convertido a entero (redondeado). */
 
-function suma(num1, num2){
+/*function suma(num1, num2){
     if (isNaN(num1) || isNaN(num2)){
         return "NaN";
     }else {
@@ -241,8 +240,34 @@ function suma(num1, num2){
 
 }
 _sumatoria = suma(10,12.5);
+console.log(_sumatoria); */
+
+/* e) Convertir la validación del ejercicio 6b) en una función separada y llamarla dentro de la función suma probando que todo siga funcionando igual. */
+
+function isNumb(num1, num2){
+    if (isNaN(num1) || isNaN(num2)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+
+function suma(num1, num2){
+    let cond = isNumb(num1, num2);
+    if (cond == true){
+        let number = validateInteger(num1);
+        let number2 = validateInteger(num2);
+        if (number == true && number2 == true){
+            return num1 + num2;
+    }else if (number == false){
+            alert(Math.round(num1));
+        }else{
+            alert(Math.round(num2));
+        }
+    }else {
+        alert("Ingrese valores numericos.");
+    }
+}
+_sumatoria = suma(10,12);
 console.log(_sumatoria);
-
-
-
-
